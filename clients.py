9c48546016dc37078b2183591b71802b8d3dce04
BixPatri <akspgoyal@gmail.com>
2022-11-21 16:53:38 +0530

    with json

--- clients.py
+++ clients.py
@@ -1,40 +1,121 @@
 import socket
 import threading
+import json
 
-#Take the input ID of the port you want to connect to
-port = 9087
-
-# Choosing Nickname
-ID = input("Enter Your ID: ")
-passwd = (input("Enter Your Password: "))
-
-# Connecting To the main load balancer server
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('localhost', port))
 
 # Listening to Server and Sending Nickname
 def receive():
     while True:
         try:
-            message = client.recv(1024).decode('ascii')
-            print(message)
-            if message == '/AUTH':
-                client.send((ID+":"+passwd).encode('ascii'))
+            message = client.recv(64).decode('ascii')
+            print(json.loads[""])
+            # if message == '/AUTH':
+            #     # client.send((ID+":"+passwd).encode('ascii'))
+            #     authenticate()
+            
         except:
-            # Close Connection When Error
             print("An error occured!")
             client.close()
             break
 
+def single_message():
+    reciepient=int(input("Reciever"))
+    message=input("Message Text")
+    mess={"type":"single_message","Reciepient":reciepient,"message":message}
+    client.send(json.dumps(mess).encode('ascii'))
+
+def group_message():
+    Group=int(input("Reciever"))
+    message=input("Message Text")
+    mess={"type":"group_message","Reciepient":Group,"message":message}
+    client.send(json.dumps(mess).encode('ascii'))
+
+def create_group():
+    g_ID=int(input("group_id"))
+    mem=input("members")
+    Members=[Credentials["ID"]]
+    for member in mem.split():
+        Members.append(int(member))
+    mess={"type":"create_group","g_ID":g_ID,"Members":Members}
+    client.send(json.dumps(mess).encode('ascii'))
+
+
+def ban():
+    g_ID=int(input("group_id"))
+    ban_ID=input("ban ID")
+    mess={"type":"ban","g_ID":g_ID,"ban_ID":ban_ID}
+    client.send(json.dumps(mess).encode('ascii'))
+
+def del_group():
+    g_ID=int(input("group_id"))
+    mess={"type":"del_group","g_ID":g_ID}
+    client.send(json.dumps(mess).encode('ascii'))
+    
+def Command(Command_type):
+    if Command_type=="create_group":
+        create_group()
+    elif Command_type=="single_message":
+        single_message()
+    elif Command_type=="group_message":
+        group_message()
+    elif Command_type=="ban":
+        ban()
+    elif Command_type=="del_group":
+        del_group()
+    else:
+        return False
+    return True
+
 # Sending Messages To Server from this client
 def write():
     while True:
-        pID = input("kisko bhejna hai? ")
-        msg = input("Are bhai kehna kya chahte ho? ")
-        message =pID+":"+msg
-        print(message)
-        client.send(message.encode('ascii'))
+        Command_type=input()
+        if Command(Command_type):
+            print("Done")
+        else:
+            print("Invalid Query")
+        # pID = input("kisko bhejna hai? ")
+        # msg = input("Are bhai kehna kya chahte ho? ")
+        # # if msg=="image":
+        # #     i=input("Image Name")
+        # #     file=open(i,'rb')
+        # #     file.read(1024)
+        # #     file
+        # message =pID+":"+msg
+        # print(message)
+        # client.send(message.encode('ascii'))
         
+        
+
+
+#Take the input ID of the port you want to connect to
+port = int(input())
+
+# Choosing Nickname
+ID = int(input("Enter Your ID: "))
+passwd = input("Enter Your Password: ")
+
+Credentials={"ID":ID,"password":passwd}
+
+# Connecting To the main load balancer server
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('localhost', port))
+        
+        
+def authenticate():
+    client.send(json.dumps(Credentials).encode('ascii'))
+
+authenticate()
+welcome_message=client.recv(1024).decode('ascii')
+print(welcome_message)
+print("""
+To message-type "single_message" then the message then the message text
+To Group_message-type "group_message" then the message then the message text
+To Create Group-type "create_group" then group id then members(space separated)
+To ban from group-type "ban" then group id and then ban id
+To delete Group- type "del_group" then group id
+""")
+
 
 # Starting Threads For Listening And Writing
 receive_thread = threading.Thread(target=receive)
--- server.py
+++ server.py
@@ -1,8 +1,9 @@
 import socket
 import threading
+import json
 
 host = 'localhost'
-port = 9087
+port = int(input())
 
 # Starting Server
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -31,57 +32,84 @@ def send_to(message,ID):
                 ID_socket[client_ID].send(message.encode("ascii"))
             return True
     return False
-    
+        
+def single_message(message,ID):
+    print(message)
+def group_message(message,ID):
+    print(message)
+def create_group(message,ID):
+    print(message)
+def ban(message,ID):
+    print(message)
+def del_group(message,ID):
+    print(message)
 # Handling Messages From Clients
 def handle(client, clientID):
     while True:
         try:
             print("FUCK")
-            message = client.recv(1024).decode('ascii')
-            print("maa")
-            ID=int(message.split(":")[0].strip())
-            print("ki")
-            msg = str(clientID)+ ":" + message.split(":")[1]
-            print(msg,ID)
-            if send_to(msg,ID):
-                client.send(f"Received by {ID}".encode("ascii"))
-                print("chut") 
+            mess = json.loads(client.recv(1024).decode('ascii'))
+            print(mess['type'])
+            print(type(mess))
+            if mess['type']=="single_message":
+                print("HI")
+                single_message(mess,clientID)
+            elif mess['type']=="group_message":
+                group_message(mess,clientID)
+            elif mess['type']=="create_group":
+                create_group(mess,clientID)
+            elif mess['type']=="ban":
+                ban(mess,clientID)
+            elif mess['type']=="del_group":
+                del_group(mess,clientID)
             else:
-                client.send(f"No such user".encode("ascii"))
-                print("vaginm")
+                print("H2I")
+                print("Invalid request")
+            print("H3I")
+            # if message
+            # ID=int(message.split(":")[0].strip())
+            # msg = message.split(":")[1]
+            # if msg[0]=="/":
+            #     command(msg)
+            #     continue
+            # msg=str(clientID)+ ":" + msg
+            # print(msg,ID)
+            # if send_to(msg,ID):
+            #     client.send(f"Received by {ID}".encode("ascii"))
+            # else:
+            #     client.send(f"No such user".encode("ascii"))
                 
         except:
             client.close()
             print(f"{client} went offline")
             break
 
+def Verify(credentials):
+    return credentials["password"]==ID_password[credentials["ID"]]
+def Add(credentials,client):
+    ID_password[credentials["ID"]]=credentials["password"]
+    ID_socket[credentials["ID"]]=client
+    
 # Receiving / Listening Function
 def receive():
     while True:
         client, address = server.accept()
-        # Request And Store Nickname
-        client.send('/AUTH'.encode('ascii'))
-        client_recv=client.recv(1024).decode('ascii')
-        ID=int(client_recv.split(":")[0].strip())
-        pass_hash = int(client_recv.split(":")[1].strip())
-        print(ID,pass_hash)
-        if ID in ID_password.keys():
-            if not ID_password[ID]==pass_hash:
-                client.send("Wrong password".encode("ascii"))
-                print("password sahi nahi dala")
+        credentials=client.recv(64).decode('ascii')
+        credentials=json.loads(credentials)
+        if credentials["ID"] in ID_password.keys():
+            if not Verify(credentials):
+                client.send("""ID already Present
+                                Wrong Password""")
                 client.close()
                 continue
             else:
-                print("OK")
+                client.send("""ID already Present
+                                Verified""")
         else:
-            ID_password[ID]=pass_hash
-            ID_socket[ID]=client
-            print("adding new")
+            Add(credentials,client)
+            client.send("""New ID created""".encode("ascii"))
         print("Connected with {}".format(str(address)))
-        client.send('You are connected to a server!'.encode('ascii'))
-
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,ID))
+        thread = threading.Thread(target=handle, args=(client,credentials["ID"]))
         thread.start()
         
 receive()
